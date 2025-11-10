901ec2f60d9389bc354c2034f1966ffc4c6b551b
MaBafk <mariaclaraagbonatini@gmail.com>
2025-11-10 16:40:14 -0300

    não ta funcionando, add algumas coisas, mas melhor revisar olhar

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ const titulo = document.getElementById('texto').innerText;
 console.log(titulo);
 alert("O texto do paragráfo é: " + titulo)
 
+const camponome = document.getElementById("campo-nome");
+camponome.addEventListener("input", (e) => {
+    console.log(e.target.value);
+});
 
+const caixa = document.getElementById("caixa");
+//caixa.innerHTML = "<h2>Conteúdo Adicionado via JavaScript</h2><p>Este conteúdo foi inserido dinamicamente.</p>";
 
-
-
+const botao = document.getElementById("btn");
+const mensagem = document.getElementById("mensagem");
+botao.addEventListener("click", (e) => {
+    const valor = campoNome.value;
+    mensagem.textContent = "Olá, " + valor;
+});
