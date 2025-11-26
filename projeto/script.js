acc498af51c1112e68aeb79daa5a1f5d6d5fd391
maria <mariaclaraagbonatini@gmail.com>
2025-11-26 16:43:07 -0300

    terminei

--- projeto/script.js
+++ projeto/script.js
@@ -1,14 +1,26 @@
-const seletorCor = document.getElementById('seletorCor');
-const btnAplicarCor = document.getElementById('btnAplicarCor');
+const seletorCorFundo = document.getElementById("seletorCor");
+const btnAplicarCor = document.getElementById("btnAplicarCor");
 
 btnAplicarCor.addEventListener("click", (e) => {
-    const cor = seletorCor.value;
+    const cor=seletorCorFundo.value;
     document.documentElement.style.setProperty("--cor-fundo", cor);
+
 });
 
 
 const chips = document.querySelectorAll(".chip");
-const galeria = document.getElementById("galeriaHero");
+const imagemDestaque = document.getElementById("galeriaHero");
 
-const imagemInicial = chips[0].dataset.imagem;
-galeria.style.backgroundImage = `url()`;
+if (chips.length > 0) {
+    const imagemInicial = chips[0].dataset.image;
+    imagemDestaque.style.backgroundImage = `url(${imagemInicial})`;
+}
+
+//forEach para cada objeto (repitir)
+chips.forEach((chip) => {
+   chip.addEventListener("click", () => {
+    imagemDestaque.classList.add("ativa");
+    imagemDestaque.style.backgroundImage = `url(${chip.dataset.image})`;
+
+   })
+}); 
